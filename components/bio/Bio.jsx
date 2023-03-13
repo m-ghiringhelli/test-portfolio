98b205d9a0ae41e4aed81a063f8540e08aed0c78
m-ghiringhelli <mghiring@gmail.com>
2023-03-13 15:59:03 -0700

    create initial layout with hero / bio

---
+++ components/bio/Bio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Bio() {
+  return (
+    <section>
+      Hi! I'm Marcus Ghiringhelli. I'm a full-stack JavaScript developer based in Portland, OR.
+    </section>
+  )
+}
---
+++ components/hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <section>
+      <p>
+        MARCUS GHIRINGHELLI
+      </p>
+    </section>
+  )
+}
